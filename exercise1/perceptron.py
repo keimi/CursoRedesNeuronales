fbe6f7cf4a6eb594cebe5267eedab82d2c522e1f
Kaminari <camrojas@ug.uchile.cl>
2017-08-02 17:31:24 -0400

    commit exercise1

---
+++ exercise1/perceptron.py
@@ -0,0 +1,17 @@
+class Perceptron:
+    w1=0.0
+    w2=0.0
+    bias=0.0
+
+    def __init__(self, w1, w2, bias):
+        self.w1 = w1
+        self.w2 = w2
+        self.bias = bias
+
+    def process_input(self, i1, i2):
+
+        if self.w1*i1 + self.w2*i2 + self.bias >= 0:
+            return 1
+        else:
+            return 0
+
